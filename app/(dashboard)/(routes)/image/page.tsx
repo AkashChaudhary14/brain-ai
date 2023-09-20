8f890574b2438e9306d34cc3f3e74c03b78bd078
Akash Chawdhery <chaudharyakash1422@gmail.com>
2023-09-20 14:50:44 +0530

    Image Generation -(Completed)

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -3,10 +3,11 @@
 import axios from "axios";
 import * as z from "zod";
 import { useState } from "react";
-import { ImageIcon } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 import { 
   Form, 
@@ -26,12 +27,10 @@ import { Heading } from "@/components/heading";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { Button } from "@/components/ui/button";
+import { Card, CardFooter } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
 import { amountOptions, formSchema, resolutionOptions } from "./constants";
-
-
-
 
 
 const ImagePage = () =>{
@@ -189,8 +188,31 @@ const ImagePage = () =>{
                   <Empty label= "No image generated."/>
                 
                )}
-                   <div>
-                    Images will be rendered here...
+                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+                    {images.map((src) =>(
+                      <Card 
+                      key={src}
+                      className="rounded-lg overflow-hidden"
+                      >
+                        <div className="relative aspect-square">
+                          <Image
+                          alt="Image"
+                          fill
+                          src={src}
+                          />
+                        </div>
+                        <CardFooter className="p-2">
+                          <Button onClick={() =>window.open(src)} 
+                          variant="secondary" 
+                          className="w-full"
+                          >
+                            <Download
+                            className="h-4 w-4 mr-2"/>
+                            Download 
+                          </Button>
+                        </CardFooter>
+                      </Card>
+                    ))}
                    </div>
                </div>
            </div>
