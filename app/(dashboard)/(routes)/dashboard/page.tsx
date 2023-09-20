8ed0e42b59d332e9f929c64fecf565e135ba2cd3
Akash Chawdhery <chaudharyakash1422@gmail.com>
2023-09-20 15:53:44 +0530

    Music Generation -(Completed)

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -57,7 +57,7 @@ const DashboardPage = () => {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the Power fo AI
+          Explore the Power of AI
         </h2>
         <p className="text-mutes-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
